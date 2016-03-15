0c8af1f30a329993e5f469fb56f771e3723b8f58
Pedro Adame Vergara <pedro.a.1smr@gmail.com>
2016-03-15 14:31:35 +0100

    Chapter 2 finished

---
+++ chap2-ruby-new/5-read/1-read-line.rb
@@ -0,0 +1,4 @@
+#Read from standard input
+puts 'Hey, write something then press enter!'
+line = gets
+puts "You wrote: #{line}"
---
+++ chap2-ruby-new/6-cli-arguments/1-argv.rb
@@ -0,0 +1,3 @@
+#Execute this one passing arguments from the command line!
+puts "You gave #{ARGV.size} arguments:"
+p ARGV
