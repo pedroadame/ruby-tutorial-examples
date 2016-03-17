3fbf34161448afded7a06a2656676874d0f15366
Pedro Adame Vergara <pedro.a.1smr@gmail.com>
2016-03-17 08:19:39 +0100

    arrays-chap4

---
+++ chap4-collections-containers-iterators/1-arrays.rb
@@ -0,0 +1,15 @@
+#Create an array using the literal
+a = [4.15, "pie", 99]
+puts a.class
+puts a.length
+puts a[0]
+puts a[1]
+puts a[3]
+
+#Using the Array class
+b = Array.new
+puts b.class
+puts b.length
+b[0] = "second"
+b[1] = "second"
+p b
