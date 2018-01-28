6ef348fb2147190b5a8d15a70acf0ced147c24ee
Pedro Adame Vergara <pedro.a.1smr@gmail.com>
2018-01-28 12:00:43 +0100

    Update ruby-example.rb

--- chap9-expressions/ruby-example.rb
+++ chap9-expressions/ruby-example.rb
@@ -1,4 +1,4 @@
-song_type = if song.mp3_type = :type_jazz
+song_type = if song.mp3_type == :type_jazz
               if song.written < Date.new 1935, 1, 1
                 :trad_jazz
               else
